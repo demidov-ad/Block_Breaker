6dd513e18179b9ae01c3de72a3281bb4c750bdb9
Alexey Demidov <kamillian@bk.ru>
2020-06-05 10:47:34 +0300

    MovementPaddle
    added some scripts for paddle and ball

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minPosX;
+    [SerializeField] float maxPosX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnit = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnit, minPosX, maxPosX);
+        transform.position = paddlePos;
+    }
+}
