8689dd2ad6e05d702df02b98991c2664611f8bbd
Alexey Demidov <kamillian@bk.ru>
2020-06-11 11:04:53 +0300

    Score
    added score and game speed

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
+
+    //cached reference
+    Level level;
+    GameStatus gameStatus;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameStatus>();
+        level.CountBreakableBlocks();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        level.BlockDestroyed();
+        gameStatus.AddToScore();
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    //config
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 2; 
+
+    //state
+    [SerializeField] int currentScore = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+    }
+
+}
