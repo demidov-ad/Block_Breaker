8eac59a039887bdb5b66702ac1e21753b278cbf2
Alexey Demidov <kamillian@bk.ru>
2020-06-12 14:26:37 +0300

    Score
    added score to ui

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     //config
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
-    [SerializeField] int pointsPerBlockDestroyed = 2; 
+    [SerializeField] int pointsPerBlockDestroyed = 2;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     //state
     [SerializeField] int currentScore = 0;
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }            
+    }
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
     void Update()
     {
         Time.timeScale = gameSpeed;
@@ -20,6 +40,7 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 
 }
