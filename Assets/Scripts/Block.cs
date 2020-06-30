b38fa99d7c17580ad8a157411d1a9dac4bff32e7
Alexey Demidov <kamillian@bk.ru>
2020-06-30 14:47:59 +0300

    Added sprites
    added func for changing sprites

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,7 @@ public class Block : MonoBehaviour
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject BlockSparklesVFX;
     [SerializeField] int maxHits;
+    [SerializeField] Sprite[] hitSprites; 
 
     // cached reference
     Level level;
@@ -36,7 +38,6 @@ public class Block : MonoBehaviour
         if (tag == "Breakable")
         {
             HandleHit();
-
         }
     }
 
@@ -47,6 +48,16 @@ public class Block : MonoBehaviour
         {
             DestroyBlock();
         }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 
     private void DestroyBlock()
