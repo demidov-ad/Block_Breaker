61b2335da59d6fb57410d050c8c06a7e8e2f159f
Alexey Demidov <kamillian@bk.ru>
2020-07-05 14:20:47 +0300

    Finished

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -8,7 +8,14 @@ public class SceneLoader : MonoBehaviour
    public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        if (currentSceneIndex == 3)
+        {
+            SceneManager.LoadScene(5);
+        }
+        else
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
     }
 
     public void LoadStartScene()
